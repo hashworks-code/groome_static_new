814f5e73888b4b8edba75a6f1c2e784706a59890
Hashworks <kumar.abhishek@hashworks.co>
2018-04-11 18:09:12 +0530

    changes for mobile

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -32,7 +32,6 @@ jQuery(document).ready(function($){
 	$('.cd-primary-nav-trigger').on('click', function(){
 		$('.cd-menu-icon').toggleClass('is-clicked'); 
 		$('.cd-header').toggleClass('menu-is-open');
-		
 		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
 		if( $('.cd-primary-nav').hasClass('is-visible') ) {
 			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
